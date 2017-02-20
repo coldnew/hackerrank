2ed6b06f5e442f981f88442fcfa34f3039fdad93
Yen-Chin Lee <coldnew.tw@gmail.com>
2017-02-20 11:34:56 +0800

    30 days -> day 7
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ 30 Days of Code/Day 7: Arrays/solution.clj
@@ -0,0 +1,19 @@
+#!/usr/bin/env boot
+
+(use '[clojure.string :only (split triml)])
+
+(let [
+      n_t (read-line)
+      n (Integer/parseInt n_t)
+      ]
+
+  (let [
+        arr_temp (read-line)
+        arr_t (split arr_temp #"\s+")
+        arr (map #(Integer/parseInt %) arr_t)
+        ]
+    ;; ------------------------------------------------------------
+    (apply println (reverse arr))
+    ;; ------------------------------------------------------------
+    )
+  )
